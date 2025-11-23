72ab029dec0ea2fde25bff067d62cc6de82b57aa
Jayson M <jayson@reeflect.fr>
2025-11-23 12:02:35 +0100

    add energy, hunger level and health to the agent

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ from llm.deepseek import DeepSeekAPI
 def main(API_KEY: str) -> None:
     generator = DeepSeekAPI("https://api.deepseek.com", "deepseek-reasoner", "You are an agent living in a nice little village.", API_KEY)
     homeZone = Zone("Park", (0, 0), (2, 2))
-    john = Agent("john", 20, generator, (0,0))
+    
+    john = Agent(
+        name="john", 
+        age=20, 
+        energy=100, 
+        health=100, 
+        hunger_level=0, 
+        brain=generator, 
+        initial_position=(0,0),
+        initial_memory=[],
+        initial_plan=[]
+    )
+    
     myWorld = World(10, 10, [homeZone], [john])
     print(myWorld.is_tile_walkable(1, 1))
     print(myWorld.get_zone_name_at(3, 2))
